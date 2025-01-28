f0ac2d2dc117a14cef5b0bd478bad2a1d54abe0e
mohamed elhadidy <melhadidy43@gmail.com>
2025-01-28 05:51:49 -0800

    create Note model class

---
+++ lib/Models/Note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int Color;
+
+  NoteModel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.Color,
+  });
+}
